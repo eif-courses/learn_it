427851047e26ecb6e6bba34bbced3ab22688dc7e
Marius <m.gzegozevskis@gmail.com>
2023-04-29 19:46:53 +0300

    migood

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,61 @@
-import {signIn, signOut, useSession} from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export const Header = () => {
-  const {data: sessionData} = useSession();
+  const { data: sessionData } = useSession();
 
-  return(
-    <div className="navbar bg-primary text-primary-content">
-      <div className="flex-1 pl-5 text-3xl font-bold">
-        {sessionData?.user?.name ? `Welcome, ${sessionData.user.name}` : ""}
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+          </label>
+          <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+            <li><a>Platform</a></li>
+            <li><a>Partners</a></li>
+            <li><a>Project Overview</a></li>
+            <li><a>Results</a></li>
+            <li><a>Contacts</a></li>
+            <li><a>Lietuviškai</a></li>
+          </ul>
+        </div>
+        <img src="/logo_pirmas.png" width="64px" height="64px"/>
+        <a className="btn btn-ghost normal-case text-xl">Simulith Auditas</a>
       </div>
-
-      <div className="flex-none gap-2">
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li><a>Platform</a></li>
+          <li><a>Partners</a></li>
+          <li><a>Project Overview</a></li>
+          <li><a>Results</a></li>
+          <li><a>Contacts</a></li>
+          <li><a>Lietuviškai</a></li>
+        </ul>
+      </div>
+      <div className="navbar-end">
         <div className="dropdown-end dropdown">
           {sessionData?.user ? (
-            <label
+            <div><label
               tabIndex={0}
               className="btn-ghost btn-circle avatar btn"
-              onClick={() => void signOut()}>
+            >
               <div className="w-10 rounded-full">
-                <img src={sessionData?.user?.image ?? ""} alt={sessionData?.user?.name ?? ""} />
+                <img src={sessionData?.user?.image ?? ""} width={64} height={64} alt={sessionData?.user?.name ?? ""} />
               </div>
             </label>
-          ):(
+              <ul tabIndex={0}
+                  className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+                <li>
+                  <a className="justify-between">
+                    Profile
+                    <span className="badge">New</span>
+                  </a>
+                </li>
+                <li><a>Settings</a></li>
+                <li onClick={() => void signOut()}><a>Logout</a></li>
+              </ul>
+            </div>
+          ) : (
             <button
               className="btn-ghost rounded-btn btn"
               onClick={() => void signIn()}>
@@ -30,8 +65,7 @@ export const Header = () => {
           }
         </div>
       </div>
-
     </div>
 
-  )
-}
+  );
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,15 @@ export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem"
+      }
+    }
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")]
 } satisfies Config;
