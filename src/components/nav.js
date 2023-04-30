84e58e822c56c6c29dc67a437f7e3381186bd44c
Marius <m.gzegozevskis@gmail.com>
2023-04-30 03:05:56 +0300

    Translation Added

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router';
 
 import en from '/locales/en';
 import fr from '/locales/fr';
+import Link from "next/link";
 
 export default function Nav() {
   const router = useRouter();
@@ -12,19 +13,16 @@ export default function Nav() {
     const locale = e.target.value;
     router.push(router.pathname, router.asPath, { locale });
   };
-
+  const options = [
+    { value: "en", label: "English" },
+    { value: "fr", label: "French" },
+    { value: "es", label: "Spanish" },
+  ];
   return (
-    <nav>
-      <div className="flex items-center justify-between bg-gray-800 text-white px-8 md:px-16 py-2">
-          <select
-            onChange={changeLanguage}
-            defaultValue={locale}
-            className="text-white text-shadow-sm text-lg bg-transparent tracking-wide"
-          >
-            <option className="text-black" value="en">EN</option>
-            <option className="text-black" value="fr">FR</option>
-          </select>
-      </div>
-    </nav>
+    <select onChange={changeLanguage}
+            defaultValue={locale} className="select">
+      <option className="text-black text-xl" value="en">English</option>
+      <option className="text-black text-xl" value="fr">France</option>
+    </select>
   );
 }
